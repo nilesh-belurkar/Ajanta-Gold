e37f9fba1fe4376d3af76c962cdc8f4b6d513871
Nilesh <nilesh.b@osmosys.co>
2025-12-12 21:22:38 +0530

    Removed unwanted files and components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,7 +13,7 @@ import { DropdownModule, SidebarModule } from '@coreui/angular';
 import { IconSetService } from '@coreui/icons-angular';
 import { routes } from './app.routes';
 
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { provideTranslateService } from '@ngx-translate/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
@@ -21,8 +21,6 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { loaderInterceptor } from './interceptor/loader.interceptor';
 import { ToastrModule } from 'ngx-toastr';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
