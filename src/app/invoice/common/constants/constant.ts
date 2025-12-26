99dfb30c6d1173bb0010141040c068535e5514c0
Nilesh <nilesh.b@osmosys.co>
2025-12-27 01:08:08 +0530

    Fixed QR code issue

--- src/app/invoice/common/constants/constant.ts
+++ src/app/invoice/common/constants/constant.ts
@@ -3,6 +3,6 @@ export const CUSTOMER_LIST_COLLECTION_NAME = 'customer_list';
 export const PRODUCT_LIST_COLLECTION_NAME = 'product_list';
 export const SAMPLE_BILL_LIST_COLLECTION_NAME = 'sample_bill_list';
 export const ORDER_LIST = 'order_list';
-export const USERS_COLLECTION_NAME = 'users';
+export const USERS_COLLECTION_NAME = 'user_list';
 
 export const DEFAULT_PAGE_NUMBER = 1;
